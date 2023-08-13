3805ed4a8e84a2f74955677fa92a9c96ba6a561d
joaoi <joaoizidio287@gmail.com>
2023-08-13 00:55:03 -0300

    Tela  inicial

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {telass as appName} from './app.json';
+
+AppRegistry.registerComponent('telass', () => App);
